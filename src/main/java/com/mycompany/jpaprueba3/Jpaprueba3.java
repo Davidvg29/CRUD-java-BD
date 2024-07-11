5d727b07225143ad372a726d04de69ce8d1b2784
David Valdez Gramajo <davidpivito@gmail.com>
2024-07-10 23:29:00 -0300

    adding poryect crud java + DB postgresql

---
+++ src/main/java/com/mycompany/jpaprueba3/Jpaprueba3.java
@@ -0,0 +1,9 @@
+package com.mycompany.jpaprueba3;
+
+import com.mycompany.jpaprueba3.persistencia.ControladoraPersistencia;
+
+public class Jpaprueba3 {
+    public static void main(String[] args) {
+        ControladoraPersistencia controlPersi = new ControladoraPersistencia();
+    }
+}
---
+++ src/main/java/com/mycompany/jpaprueba3/persistencia/ControladoraPersistencia.java
@@ -0,0 +1,5 @@
+package com.mycompany.jpaprueba3.persistencia;
+
+public class ControladoraPersistencia {
+    AlumnoJpaController aluJpa = new AlumnoJpaController();
+}
